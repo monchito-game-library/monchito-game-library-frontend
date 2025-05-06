73043087b9ef0f475f8112de87f93f66d441194d
Alberto Checa Muelas <alcheca@mercadona.es>
2025-05-06 17:21:23 +0200

    feat: add transloco to proyect

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,30 +2,62 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
-import { MatSlideToggle } from '@angular/material/slide-toggle';
 import { ThemeService } from '../../services/theme.service';
+import { TranslocoPipe, TranslocoService } from '@ngneat/transloco';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatSelect, MatSelectTrigger } from '@angular/material/select';
+import { MatOption } from '@angular/material/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatTooltip } from '@angular/material/tooltip';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { availableLangConstant } from '../../models/constants/available-lang.constant';
+import { AvailableLangInterface } from '../../models/interfaces/available-lang.interface';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCard, MatCardTitle, MatCardContent, RouterLink, MatIcon, MatSlideToggle],
+  imports: [
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    RouterLink,
+    MatIcon,
+    TranslocoPipe,
+    MatFormField,
+    MatLabel,
+    MatSelect,
+    MatSelectTrigger,
+    MatOption,
+    MatIconButton,
+    MatTooltip,
+    ReactiveFormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-  isDark: boolean = false;
+  private _themeService: ThemeService = inject(ThemeService);
+  private _translocoService: TranslocoService = inject(TranslocoService);
 
-  private themeService: ThemeService = inject(ThemeService);
+  isDark: boolean = false;
+  availableLanguages: AvailableLangInterface[] = availableLangConstant
+  selectedLangControl: FormControl<string | null> = new FormControl(this._translocoService.getActiveLang());
 
   ngOnInit(): void {
-    this.themeService.initTheme();
-    this.isDark = this.themeService.isDarkTheme();
+    this._themeService.initTheme();
+    this.isDark = this._themeService.isDarkTheme();
+
+    this.selectedLangControl.valueChanges.subscribe((lang: string | null) => {
+      if (lang) {
+        this._translocoService.setActiveLang(lang);
+      }
+    });
   }
 
   toggleTheme(): void {
     this.isDark = !this.isDark;
     this.isDark
-      ? this.themeService.setDarkTheme()
-      : this.themeService.setLightTheme();
+      ? this._themeService.setDarkTheme()
+      : this._themeService.setLightTheme();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-import {applySavedTheme} from './app/apply-saved.theme';
+import { applySavedTheme } from './app/apply-saved.theme';
 
 applySavedTheme();
 
