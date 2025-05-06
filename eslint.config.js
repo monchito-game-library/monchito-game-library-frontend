609ea59b0959cd9fb14f6b00e46e1b21cd4a8e84
Espinilleitor <108438583+AlbertoChecaMuelas@users.noreply.github.com>
2025-05-06 12:36:52 +0200

    feat: add lint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+// eslint.config.js
+const angular = require('@angular-eslint/eslint-plugin');
+const tseslint = require('typescript-eslint');
+const parser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@angular-eslint': angular
+    },
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        { type: 'attribute', prefix: 'app', style: 'camelCase' }
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        { type: 'element', prefix: 'app', style: 'kebab-case' }
+      ]
+    }
+  },
+  {
+    files: ['**/*.html'],
+    plugins: {
+      '@angular-eslint/template': require('@angular-eslint/eslint-plugin-template')
+    },
+    languageOptions: {
+      parser: require('@angular-eslint/template-parser')
+    },
+    rules: {
+      '@angular-eslint/template/banana-in-box': 'error',
+      '@angular-eslint/template/no-negated-async': 'warn'
+    }
+  }
+];
