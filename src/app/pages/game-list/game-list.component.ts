8d81602b8ba855b02550839bf04b5e74a1903509
alber <djalberto28@gmail.com>
2025-05-07 02:00:50 +0200

    feat: rework cards

--- src/app/pages/game-list/game-list.component.ts
+++ src/app/pages/game-list/game-list.component.ts
@@ -1,22 +1,22 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GameInterface } from '../../models/interfaces/game.interface';
-import { FormsModule } from '@angular/forms';
-import { IndexedDBRepository } from '../../repositories/indexeddb.repository';
-import { GamesConsoleType } from '../../models/types/games-console.type';
-import { MatInput } from '@angular/material/input';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatSelect } from '@angular/material/select';
-import { MatOption } from '@angular/material/core';
-import { MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
-import { MatIconButton } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
-import { MatIcon } from '@angular/material/icon';
+import {Component, computed, inject, OnInit, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {GameInterface} from '../../models/interfaces/game.interface';
+import {FormsModule} from '@angular/forms';
+import {IndexedDBRepository} from '../../repositories/indexeddb.repository';
+import {GamesConsoleType} from '../../models/types/games-console.type';
+import {MatInput} from '@angular/material/input';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatSelect} from '@angular/material/select';
+import {MatOption} from '@angular/material/core';
+import {MatIconButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
+import {MatIcon} from '@angular/material/icon';
+import {GameCardComponent} from '../../components/game-card/game-card.component';
 
 @Component({
   selector: 'app-game-list',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatFormField, MatLabel, MatInput, MatSelect, MatOption, MatCard, MatCardHeader, MatIconButton, RouterLink, MatIcon, MatCardContent, MatCardSubtitle, MatCardTitle],
+  imports: [CommonModule, FormsModule, MatFormField, MatLabel, MatInput, MatSelect, MatOption, MatIconButton, RouterLink, MatIcon, GameCardComponent],
   templateUrl: './game-list.component.html',
   styleUrls: ['./game-list.component.scss']
 })
