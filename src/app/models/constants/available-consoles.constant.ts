569564dfdf5425378a11a0ee1734c769241b9185
Alberto Checa Muelas <alcheca@mercadona.es>
2025-05-08 15:58:41 +0200

    feat: refactor changes

---
+++ src/app/models/constants/available-consoles.constant.ts
@@ -0,0 +1,23 @@
+import { AvailableConsolesInterface } from '../interfaces/available-consoles.interface';
+
+export const availableConsolesConstant: AvailableConsolesInterface[] = [
+  { code: 'PS5', labelKey: 'consoles.ps5' },
+  { code: 'PS4', labelKey: 'consoles.ps4' },
+  { code: 'PS3', labelKey: 'consoles.ps3' },
+  { code: 'PS2', labelKey: 'consoles.ps2' },
+  { code: 'PS1', labelKey: 'consoles.ps1' },
+  { code: 'PS-VITA', labelKey: 'consoles.psVita' },
+  { code: 'PSP', labelKey: 'consoles.psp' },
+  { code: 'GBC', labelKey: 'consoles.gbc' },
+  { code: 'GBA', labelKey: 'consoles.gba' },
+  { code: 'DS', labelKey: 'consoles.ds' },
+  { code: '3DS', labelKey: 'consoles.3ds' },
+  { code: 'GAME-CUBE', labelKey: 'consoles.gameCube' },
+  { code: 'WII', labelKey: 'consoles.wii' },
+  { code: 'SWITCH', labelKey: 'consoles.switch' },
+  { code: 'XBOX', labelKey: 'consoles.xbox' },
+  { code: 'XBOX-360', labelKey: 'consoles.xbox360' },
+  { code: 'XBOX-ONE', labelKey: 'consoles.xboxOne' },
+  { code: 'XBOX-SERIES', labelKey: 'consoles.xboxSeries' },
+  { code: 'PC', labelKey: 'consoles.pc' }
+];
---
+++ src/app/models/constants/game-library.constant.ts
@@ -0,0 +1,3 @@
+export const imagePlatinumPath = 'assets/images/platinum-trophy.png';
+export const imageTrophyHiddenPath = 'assets/images/hidden-trophy.webp';
+export const defaultGameCover = 'assets/images/default-game-cover.png';
