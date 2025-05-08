607190a66c8c87bc6f7f05f0d823fa38be42a80a
Espinilleitor <108438583+AlbertoChecaMuelas@users.noreply.github.com>
2025-05-09 00:37:44 +0200

    feat: update games

--- src/app/models/interfaces/available-lang.interface.ts
+++ src/app/models/interfaces/available-lang.interface.ts
@@ -1,4 +1,4 @@
 export interface AvailableLangInterface {
-  code: string; // Código del idioma (ej. 'en', 'es')
-  labelKey: string; // Clave de traducción para el idioma (ej. 'home.language.option.en')
+  code: string;
+  labelKey: string;
 }
