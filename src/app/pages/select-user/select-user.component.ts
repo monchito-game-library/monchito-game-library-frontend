e05ff7aa6645be2ed1e8446541562565713cbd56
alber <djalberto28@gmail.com>
2025-05-10 18:20:11 +0200

    feat: rework proyect

--- src/app/pages/select-user/select-user.component.ts
+++ src/app/pages/select-user/select-user.component.ts
@@ -2,15 +2,26 @@ import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserContextService } from '../../services/user-context.service';
 import { MatButtonModule } from '@angular/material/button';
-import { MatChipListbox, MatChipOption } from '@angular/material/chips';
+import { MatChipAvatar, MatChipListbox, MatChipOption } from '@angular/material/chips';
 import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
 import { TranslocoPipe } from '@ngneat/transloco';
 import { availableUsers } from '../../models/constants/available-users.constant';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-select-user',
   standalone: true,
-  imports: [MatButtonModule, MatCard, MatCardTitle, MatCardContent, TranslocoPipe, MatChipListbox, MatChipOption],
+  imports: [
+    MatButtonModule,
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    TranslocoPipe,
+    MatChipListbox,
+    MatChipOption,
+    NgOptimizedImage,
+    MatChipAvatar
+  ],
   templateUrl: './select-user.component.html',
   styleUrl: './select-user.component.scss'
 })
