408ffb3e59e67115ea3505f191ce80c72bebda81
Alberto Checa Muelas <alcheca@mercadona.es>
2026-03-21 23:18:43 +0100

    fix(stores): remove unused MatIconButton and MatTooltip imports from edit panel

--- src/app/presentation/pages/management/stores/stores-management.component.ts
+++ src/app/presentation/pages/management/stores/stores-management.component.ts
@@ -11,7 +11,7 @@ import {
 } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
-import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatFormField, MatLabel, MatError } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
@@ -19,7 +19,6 @@ import { MatSelect } from '@angular/material/select';
 import { MatOption } from '@angular/material/core';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { MatDialog } from '@angular/material/dialog';
-import { MatTooltip } from '@angular/material/tooltip';
 import { TranslocoPipe, TranslocoService } from '@jsverse/transloco';
 
 import { STORE_USE_CASES, StoreUseCasesContract } from '@/domain/use-cases/store/store.use-cases.contract';
@@ -56,9 +55,7 @@ interface StoreFormResult {
     MatSelect,
     MatOption,
     MatButton,
-    MatIconButton,
     MatIcon,
-    MatTooltip,
     TranslocoPipe
   ],
   template: `
