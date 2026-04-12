66dd40a34acc64300f1944079b5ab36628389a80
Alberto Checa Muelas <alcheca@mercadona.es>
2026-04-12 18:58:15 +0200

    fix: devolver userPreferences a root al ser necesario en el arranque de la app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,10 +11,12 @@ import { provideTransloco } from '@jsverse/transloco';
 import { authRepositoryProvider } from '@/di/repositories/auth.repository.provider';
 import { rawgRepositoryProvider } from '@/di/repositories/rawg.repository.provider';
 import { storeRepositoryProvider } from '@/di/repositories/store.repository.provider';
+import { userPreferencesRepositoryProvider } from '@/di/repositories/user-preferences.repository.provider';
 import { wishlistRepositoryProvider } from '@/di/repositories/wishlist.repository.provider';
 import { authUseCasesProvider } from '@/di/use-cases/auth.use-cases.provider';
 import { catalogUseCasesProvider } from '@/di/use-cases/catalog.use-cases.provider';
 import { storeUseCasesProvider } from '@/di/use-cases/store.use-cases.provider';
+import { userPreferencesUseCasesProvider } from '@/di/use-cases/user-preferences.use-cases.provider';
 import { wishlistUseCasesProvider } from '@/di/use-cases/wishlist.use-cases.provider';
 import { provideServiceWorker } from '@angular/service-worker';
 
@@ -42,6 +44,8 @@ export const appConfig: ApplicationConfig = {
     catalogUseCasesProvider,
     storeRepositoryProvider,
     storeUseCasesProvider,
+    userPreferencesRepositoryProvider,
+    userPreferencesUseCasesProvider,
     wishlistRepositoryProvider,
     wishlistUseCasesProvider,
     provideServiceWorker('ngsw-worker.js', {
--- src/app/presentation/pages/settings/settings.routes.ts
+++ src/app/presentation/pages/settings/settings.routes.ts
@@ -1,12 +1,9 @@
 import { Routes } from '@angular/router';
 import { canActivateUser } from '@/guards/user.guard';
-import { userPreferencesRepositoryProvider } from '@/di/repositories/user-preferences.repository.provider';
-import { userPreferencesUseCasesProvider } from '@/di/use-cases/user-preferences.use-cases.provider';
 
 export const settingsRoutes: Routes = [
   {
     path: '',
-    providers: [userPreferencesRepositoryProvider, userPreferencesUseCasesProvider],
     loadComponent: () => import('./settings.component').then((m) => m.SettingsComponent),
     canActivate: [canActivateUser]
   }
